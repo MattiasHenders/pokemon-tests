500848a6a0e6add077519ea6a21045d119b8bbfd
MattiasHenders <henders.mattias@gmail.com>
2024-11-26 20:57:20 -0800

    Add exciting modal

--- src/components/pages/home/GameDoneModal/index.tsx
+++ src/components/pages/home/GameDoneModal/index.tsx
@@ -4,13 +4,17 @@ import Modal from '@mui/material/Modal'
 import Fade from '@mui/material/Fade'
 import Typography from '@mui/material/Typography'
 import { palette } from '@/styles/palette'
-import Link from 'next/link'
 import { useGameTypeStore } from '@/src/stores/game'
 import { useAuthenticator } from '@aws-amplify/ui-react'
+import Image from 'next/image'
+import StyledButton from '@/src/components/common/StyledButton'
+import { useRouter } from 'next/router'
+import Countdown from './Countdown'
 
 export default () => {
   const { gameOverModalOpen, setGameOverModalOpen } = useGameTypeStore()
   const { user } = useAuthenticator()
+  const router = useRouter()
   return (
     <div>
       <Modal
@@ -44,32 +48,67 @@ export default () => {
               id="transition-modal-title"
               variant="h6"
               component="h2"
-              sx={{ color: palette.primary.darkText }}
+              sx={{
+                color: palette.primary.darkText,
+                textAlign: 'center',
+                fontSize: { xs: 20, sm: 24, md: 28 },
+              }}
             >
               Game Over
             </Typography>
             <Typography
               id="transition-modal-description"
-              sx={{ mt: 2, color: palette.primary.darkText }}
+              sx={{
+                color: palette.primary.darkText,
+                textAlign: 'center',
+              }}
             >
-              Thank you so much for playing!
+              Thank you for playing today&apos;s game! Continue your streak
+              tomorrow!
             </Typography>
+            <Countdown />
             {user ? (
-              <Box sx={{ my: 2 }}>
-                <Link color={palette.primary.lightText} href={'/profile'}>
-                  Click here to view your stats and achievements!
-                </Link>
+              <Box sx={{ my: 2, display: 'flex', justifyContent: 'center' }}>
+                <StyledButton
+                  variant="contained"
+                  size="large"
+                  onClick={() => router.push('/profile')}
+                >
+                  View your stats!
+                </StyledButton>
               </Box>
             ) : (
               <>
                 <Typography sx={{ mb: 2, color: palette.primary.darkText }}>
                   Want to save your scores, get acheivements, and more?
                 </Typography>
-                <Link color={palette.primary.lightText} href={'/auth'}>
-                  Click here to sign up or login!
-                </Link>
+                <StyledButton
+                  variant="contained"
+                  size="large"
+                  onClick={() => router.push('/auth')}
+                >
+                  Click here to sign up!
+                </StyledButton>
               </>
             )}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                mt: 2,
+                position: 'relative',
+                width: '100%',
+                // height: 259,
+                aspectRatio: 500 / 259,
+              }}
+            >
+              <Image
+                src="/img/misc/team.gif"
+                alt="team gif"
+                fill
+                style={{ objectFit: 'contain' }}
+              />
+            </Box>
           </Box>
         </Fade>
       </Modal>
