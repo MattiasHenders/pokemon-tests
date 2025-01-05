cd8038e0905048027205ca98e7319d7642b27b5e
MattiasHenders <henders.mattias@gmail.com>
2025-01-05 13:00:19 -0800

    Change email time

--- amplify/functions/notifications/notifyUsersDaily/resource.ts
+++ amplify/functions/notifications/notifyUsersDaily/resource.ts
@@ -5,6 +5,6 @@ export const notifyUsersDaily = defineFunction({
   name: 'notify-users-daily',
   // optionally specify a path to your handler (defaults to "./handler.ts")
   entry: './handler.ts',
-  schedule: '0 12 * * ? *',
+  schedule: '0 1 * * ? *',
   timeoutSeconds: 45,
 })
